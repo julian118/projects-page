0e95ca519820e2ce82e52fe4772fff2702ff7546
julian <juliankroes2206@gmail.com>
2024-03-09 13:22:26 +0100

    feat: Added vercel site analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
+import { APP_INITIALIZER} from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
+import { inject } from "@vercel/analytics";
+ 
+import { AppComponent } from "./app/app.component";
+import { routes } from "./app/app.routes";
+ 
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        inject({ debug: false})
+      }
+    }
+  ]
+})
+  .catch(err => console.error(err));
+
+/*
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  */
